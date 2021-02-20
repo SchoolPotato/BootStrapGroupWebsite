5aa4b14b879958123f1eaf3443f20d97c0659dff
Alexander Welton <Awelto389@west-mec.local>
2021-02-19 18:17:23 -0700

    Added commenting the the JS, started functions for changing the retro theme on other pages

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,5 @@
-//Get all the elements I want to change on the History page
+// As far as I (Alex) am aware, all of this code is mine and any credit for the atrocities against humanity I've commited in here should thusly be given to me
+// Get all the elements I want to change on the History page
 if(document.getElementsByTagName("body")[0].id == "history"){
     var body = document.getElementsByTagName("body")[0];
     var headerOne = document.getElementById("modernHeader1");
@@ -11,7 +12,7 @@ if(document.getElementsByTagName("body")[0].id == "history"){
     var navbarLink3 = document.getElementById("modern3");
     var gamecubeText = document.getElementsByClassName("gamecube");
 }
-//Get all the elements I want to change on the Home page
+// Get all the elements I want to change on the Home page
 if(document.getElementsByTagName("body")[0].id == "home"){
     var body = document.getElementsByTagName("body")[0];
     var gamecubeText1 = document.getElementById("modern-gamecube1");
@@ -23,7 +24,7 @@ if(document.getElementsByTagName("body")[0].id == "home"){
     var navbarLink2 = document.getElementById("modern2");
     var navbarLink3 = document.getElementById("modern3");
 }
-//Get all the elements I want to change on the Gallery page
+// Get all the elements I want to change on the Gallery page
 if(document.getElementsByTagName("body")[0].id == "gallery"){
     var body = document.getElementsByTagName("body")[0];
     var gamecubeText1 = document.getElementById("modern-gamecube1");
@@ -37,9 +38,9 @@ if(document.getElementsByTagName("body")[0].id == "gallery"){
 }
 
 
-//The way I've done this is actually trash but it works and I'd like to think that's what matters
+// The way I've done this is actually trash but it works and I'd like to think that's what matters
 function retroThemeHistory(){
-    //Toggle between retro ids and modern ids
+    // If the body's id isn't retro, make it retro and change a bunch of other elements to retro ids
     if(body.id != "retro"){
         body.id = "retro";
         headerOne.id = "retro1";
@@ -51,12 +52,14 @@ function retroThemeHistory(){
         navbarLink2.id = "neon2";
         navbarLink3.id = "neon3";
         
+        // These for loops are just to make sure all of the side "anecdotes" or whatever they're called as well as both of the "GameCube" brand texts work
         for(i=0; i<images.length; i++){
             images[i].id="retro";
         }
         for(i=0; i<gamecubeText.length; i++){
             gamecubeText[i].id="retro-gamecube1";
         }
+        // If the body IS retro, change it back to modern as well as al the elements that can be swapped to retro
     } else {
         body.id = "modern";
         headerOne.id = "modern1";
@@ -68,6 +71,7 @@ function retroThemeHistory(){
         navbarLink2.id = "modern2";
         navbarLink3.id = "modern3";
 
+        // See previous for loop comment on line 55
         for(i=0; i<images.length; i++){
             images[i].id="imageSection1";
         }
@@ -75,4 +79,12 @@ function retroThemeHistory(){
             gamecubeText[i].id="modern-gamecube1";
         }
     }
+}
+
+function retroThemeGallery(){
+    // Will make the gallery's retro theme and get back to this
+}
+
+function retroThemeHome(){
+    // Will make the home page's retro theme and get back to this
 }
