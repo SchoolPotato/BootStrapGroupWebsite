2d44686ae0e9ccdc9e71d95cb51010b2be534476
Alexander Welton <Awelto389@west-mec.local>
2021-02-22 16:52:52 -0700

    Retro code added to all pages

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,5 @@
-// As far as I (Alex) am aware, all of this code is mine and any credit for the atrocities against humanity I've commited in here should thusly be given to me
+// As far as I (Alex) am aware, all of this code is mine and therefore I apologize for any and all sins against humanity I've committed here
+
 // Get all the elements I want to change on the History page
 if(document.getElementsByTagName("body")[0].id == "history"){
     var body = document.getElementsByTagName("body")[0];
@@ -15,26 +16,25 @@ if(document.getElementsByTagName("body")[0].id == "history"){
 // Get all the elements I want to change on the Home page
 if(document.getElementsByTagName("body")[0].id == "home"){
     var body = document.getElementsByTagName("body")[0];
-    var gamecubeText1 = document.getElementById("modern-gamecube1");
-    var gamecubeText2 = document.getElementById("modern-gamecube2");
-    var navbar = document.getElementsByTagName("nav")[0];
-    var footer = document.getElementsByTagName("nav")[1];
-    var logo = document.getElementById("logo");
+    var gamecubeText = document.getElementsByClassName("gamecube");
     var navbarLink1 = document.getElementById("nav-text-dark");
     var navbarLink2 = document.getElementById("modern2");
     var navbarLink3 = document.getElementById("modern3");
+    var navbar = document.getElementsByTagName("nav");
+    var logo = document.getElementById("logo");
+    var cards = [document.getElementById("card1"), document.getElementById("card2"), document.getElementById("card3")];
+    var specsHeader = document.getElementById("normal");
 }
 // Get all the elements I want to change on the Gallery page
 if(document.getElementsByTagName("body")[0].id == "gallery"){
     var body = document.getElementsByTagName("body")[0];
-    var gamecubeText1 = document.getElementById("modern-gamecube1");
-    var gamecubeText2 = document.getElementById("modern-gamecube2");
-    var navbar = document.getElementsByTagName("nav")[0];
-    var footer = document.getElementsByTagName("nav")[1];
-    var logo = document.getElementById("logo");
+    var gamecubeText = document.getElementsByClassName("gamecube");
     var navbarLink1 = document.getElementById("nav-text-dark");
     var navbarLink2 = document.getElementById("modern2");
     var navbarLink3 = document.getElementById("modern3");
+    var navbar = document.getElementsByTagName("nav");
+    var logo = document.getElementById("logo");
+    var header = document.getElementById("modern-header");
 }
 
 
@@ -81,10 +81,79 @@ function retroThemeHistory(){
     }
 }
 
+// The two functions below are the same function with different variables
+// This is honestly the fastest way I could think of to do this that didn't throw any errors
+// See line 1 for my apology
 function retroThemeGallery(){
-    // Will make the gallery's retro theme and get back to this
+    if(body.id != "retro"){
+        body.id = "retro";
+        navbar[0].id = "retro1";
+        navbar[1].id = "retro2";
+        logo.setAttribute("src", "Images/gamecube-logo-retro.png")
+        logo.setAttribute("style", "margin-bottom: 2.25rem;");
+        navbarLink1.id = "neon1";
+        navbarLink2.id = "neon2";
+        navbarLink3.id = "neon3";
+
+        for(i=0; i<gamecubeText.length; i++){
+            gamecubeText[i].id="retro-gamecube1";
+        }
+
+        header.id = "neon-header"
+    } else {
+        body.id = "modern";
+        navbar[0].id = "fixed"
+        navbar[1].id = "regular"
+        logo.setAttribute("src", "Images/gamecube-logo.png");
+        navbarLink1.id = "nav-text-dark";
+        navbarLink2.id = "modern2";
+        navbarLink3.id = "modern3";
+        logo.setAttribute("style", "");
+
+        for(i=0; i<gamecubeText.length; i++){
+            gamecubeText[i].id="modern-gamecube1";
+        }
+
+        header.id = "modern-header"
+    }
 }
 
 function retroThemeHome(){
-    // Will make the home page's retro theme and get back to this
+    if(body.id != "retroHome"){
+        body.id = "retroHome";
+        navbar[0].id = "retro1";
+        navbar[1].id = "retro2";
+        logo.setAttribute("src", "Images/gamecube-logo-retro.png")
+        logo.setAttribute("style", "margin-bottom: 2.49rem;");
+        navbarLink1.id = "neon1";
+        navbarLink2.id = "neon2";
+        navbarLink3.id = "neon3";
+        specsHeader.id = "retro";
+        
+        for(i=0; i<gamecubeText.length; i++){
+            gamecubeText[i].id="retro-gamecube1";
+        }
+
+        for(i=0; i<cards.length; i++){
+            cards[i].id="brick-texture"
+        }
+    } else {
+        body.id = "modern";
+        navbar[0].id = "fixed"
+        navbar[1].id = "regular"
+        logo.setAttribute("src", "Images/gamecube-logo.png");
+        navbarLink1.id = "nav-text-dark";
+        navbarLink2.id = "modern2";
+        navbarLink3.id = "modern3";
+        logo.setAttribute("style", "");
+        specsHeader.id = "normal";
+
+        for(i=0; i<gamecubeText.length; i++){
+            gamecubeText[i].id="modern-gamecube1";
+        }
+
+        for(i=0; i<cards.length; i++){
+            cards[i].id="no-texture"
+        }
+    }
 }
